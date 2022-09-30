e738f0465948288eed1dcf961ba0ded60df70295
zhoutaipiing <1164021461@qq.com>
2022-09-30 16:39:47 +0800

    edit ui

--- src/main.js
+++ src/main.js
@@ -1,37 +1,36 @@
-import Vue from 'vue'
-import App from './App'
-import store from './store'
-import router from './router'
-import Element from 'element-ui'
-import Cookies from 'js-cookie'
-import * as filters from './filters' // global filters
+import Vue from "vue";
+import App from "./App";
+import store from "./store";
+import router from "./router";
+import Element from "element-ui";
+import Cookies from "js-cookie";
+import * as filters from "./filters"; // global filters
 
-import '@/core/use'
-import '@/core/use-console'
-import '@/core/use-stage'
+import "@/core/use";
+import "@/core/use-console";
+import "@/core/use-stage";
 
-import 'normalize.css/normalize.css' // a modern alternative to CSS resets
-import '@/styles/theme/index.css' // global css
-import '@/styles/index.scss' // global css
+import "normalize.css/normalize.css"; // a modern alternative to CSS resets
+import "@/styles/theme/index.css"; // global css
+import "@/styles/index.scss"; // global css
 
-import './icons' // icon
-import './permission' // permission control
-
+import "./icons"; // icon
+import "./permission"; // permission control
 
 Vue.use(Element, {
-  size: Cookies.get('size') || 'medium' // set element-ui default size
-})
+  size: Cookies.get("size") || "small", // set element-ui default size
+});
 
 // register global utility filters
-Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-})
+Object.keys(filters).forEach((key) => {
+  Vue.filter(key, filters[key]);
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
-  render: h => h(App)
-})
+  render: (h) => h(App),
+});
